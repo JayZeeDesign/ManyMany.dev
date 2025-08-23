ebde8dcd9292a07d805d45041625d568a7e7b9c0
Jason Zhou <jason.zhou.design@gmail.com>
2025-08-24 00:13:30 +1000

    Add updater plugin and configure automatic updates - Add tauri-plugin-updater dependency - Configure updater endpoints and public key - Add updater permissions for checking and installing updates - Ready for GitHub Actions release workflow

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -20,6 +20,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .manage(Mutex::new(terminal_manager))
         .invoke_handler(tauri::generate_handler![
             add_project,
